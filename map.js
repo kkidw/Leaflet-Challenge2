fe827f35b43fc22a4e242b57047fe6b0c90cd751
kkidw <102126819+kkidw@users.noreply.github.com>
2022-07-25 14:41:17 -0500

    Create map.js

---
+++ map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([51.505, -0.09], 2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
